9e6399d755712951b67c32589553cd4b9b397e0a
Daniel Lamarr <danielchidiebele@gmail.com>
2020-04-30 16:15:59 +0100

    initialize project

---
+++ src/graphql/index.js
@@ -0,0 +1,13 @@
+import {graphql, buildSchema} from 'graphql';
+
+export const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+export const root = {
+  hello: () => {
+    return 'Yipah Challenge!';
+  },
+};
